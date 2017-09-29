dc222f9c6b00c573ee6e1d37099d852fce4f576b
Nikhil Devshatwar <niksdevice@gmail.com>
2017-09-29 03:13:14 -0700

    Initial commit for imagemagic server
    Signed-off-by: Nikhil Devshatwar <niksdevice@gmail.com>

---
+++ README.md
@@ -0,0 +1,26 @@
+# Nodejs experiments
+All trial example codes that I developed while learning nodejs and web technologies.
+
+**WARNING** This is amatuer level coding. This might be completely off the standards, inefficient and totally unsecure code. Do not use it for million doller systems.
+
+## ImageMagic server
+Simple http server to experiment with ImageMagick Node.js module
+This is supposed to use server side computing to process images.
+
+Install the required modules and packages
+``` bash
+sudo apt-get install nodejs npm
+npm install imagemagick
+```
+
+
+
+## More to come
+
+::: warning
+*here be dragons*
+
+I edited this on https://markdown-it.github.io/
+Liked the dragons warning, so kept it
+:::
+ 
---
+++ imserver.js
@@ -0,0 +1,46 @@
+var http = require('http');
+var url = require('url');
+var im = require('imagemagick');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+	if (req.url.match("/IM")) {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write('Image Magick');
+
+		var q = url.parse(req.url, true).query;
+		console.log(q);
+		console.log("Converting imagemagick " + q.src);
+
+		im.convert([q.src, '-resize', q.size, 'output.jpg'], 
+		function(err, stdout){
+			if (err) {
+				res.write('<br/><h1>Failed to convert</h1>');
+				res.end();
+			} else {
+				res.write('<img src="localhost:8080/img/output.jpg"></img>');
+				res.end();
+				console.log('>> Conversion done');
+			}
+		});
+
+	} else if (req.url.match("/img/")) {
+		var fname = req.url.slice(5)
+		console.log("Reading file " + fname);
+
+		fs.readFile(fname, function(err, data) {
+			if (err) {
+				res.writeHead(404, {'Content-Type': 'text/html' });
+		 		res.end();
+			} else {
+				res.writeHead(200, {'Content-Type': 'image/gif' });
+		 		res.end(data, 'binary');
+		 	}
+	 	});
+
+	} else {
+		res.writeHead(404, {'Content-Type': 'text/html'});
+		res.write('Page not found "' + req.url + '"');
+		res.end();
+	}
+}).listen(8080);
